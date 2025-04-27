a1ea9bce3bb10e9089124b51569562a32d6168b6
Georgi Lavchanski <149933409+georgeexe9@users.noreply.github.com>
2025-04-27 10:06:54 +0300

    Adding TaskList, delete, toggletask functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import Navbar from './Navbar'
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import TaskForm from './TaskForm';
+import TaskList from './TaskList';
 import "./App.css";
 
 function App() {
 
-  const [setTasks] = useState([]);
+  const [tasks,setTasks] = useState([]);
   function addTask(text) {
     const newTask = {
       id: Date.now(),
@@ -15,18 +16,41 @@ function App() {
     };
     setTasks((prev) => [newTask, ...prev]);
   }
+  function toggleTask(id) {
+    setTasks((prev) =>
+      prev.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  }
+  function deleteTask(id) {
+    setTasks((prev) => prev.filter((task) => task.id !== id));
+  }
 
   return (
-      <Router>
-        <div className="app-container">
-          <Navbar />
-          <div className="task-wrapper">
-          <TaskForm addTask={addTask} />
-          </div>
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <div className="task-wrapper">
+        <TaskForm addTask={addTask} />
+        <Routes>
+            <Route
+              path="/"
+              element={<TaskList view="todo" tasks={tasks} toggleTask={toggleTask} deleteTask={deleteTask} />}
+            />
+            <Route
+              path="/completed"
+              element={<TaskList view="completed" tasks={tasks} toggleTask={toggleTask} deleteTask={deleteTask} />}
+            />
+            <Route
+              path="/uncompleted"
+              element={<TaskList view="uncompleted" tasks={tasks} toggleTask={toggleTask} deleteTask={deleteTask} />}
+            />
+          </Routes>
         </div>
-      </Router>
-    );
-  
+      </div>
+    </Router>
+  );
 }
 
 export default App
---
+++ src/TaskList.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./TaskList.css";
+
+function TaskList({ view, tasks, toggleTask, deleteTask }) {
+  const filteredTasks = tasks.filter((task) => {
+    if (view === "completed") return task.completed;
+    if (view === "uncompleted") return !task.completed;
+    return true; 
+  });
+
+  return (
+    <ul className="task-list">
+      {filteredTasks.map((task) => (
+        <li key={task.id} className="task-item">
+          <span
+            onClick={() => toggleTask(task.id)}
+            style={{ textDecoration: task.completed ? "line-through" : "none", cursor: "pointer" }}
+          >
+            {task.text}
+          </span>
+          <button className="delete-button" onClick={() => deleteTask(task.id)}>
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default TaskList;
