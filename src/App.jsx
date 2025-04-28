5dc10858b2f02ad141454354291dd2280350e35d
Georgi Lavchanski <149933409+georgeexe9@users.noreply.github.com>
2025-04-28 09:05:37 +0300

    Adding a header , and notes examples

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import Navbar from './Navbar'
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import TaskForm from './TaskForm';
 import TaskList from './TaskList';
+import Header from './Header'
 import "./App.css";
+
+
 
 function App() {
 
-  const [tasks,setTasks] = useState([]);
+  const [tasks,setTasks] = useState([
+    {id:1, text:"Finish your react project due 20 May", completed: false},
+    {id: 2, text:"Finish your c# project due 1 june", completed: false},
+    {id: 3, text:"Register due 10 september", completed: false},
+  ]);
   function addTask(text) {
     const newTask = {
       id: Date.now(),
@@ -32,7 +39,9 @@ function App() {
       <div className="app-container">
         <Navbar />
         <div className="task-wrapper">
+        <Header></Header>
         <TaskForm addTask={addTask} />
+        
         <Routes>
             <Route
               path="/"
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+function Header() {
+
+  return (
+        <header className="header">
+          <h1 className="header-title">Take a note!📝</h1>
+          <p className="header-subtitle">Welcome! To begin, write a note below</p>
+        </header>
+  );
+
+
+}
+export default Header
